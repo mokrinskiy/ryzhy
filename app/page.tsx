1d606090d6a6b437741286a7a4b8fe90e613bd95
mokrinskiy <artyommokrinskiy@gmail.com>
2025-02-06 17:50:10 +0700

    main fix and animate

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { menu_links } from "@/content/menu_links";
+import { motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="w-full h-screen max-lg:h-auto px-[30px] py-[10px] max-md:p-[10px] flex flex-col md:justify-between overflow-hidden">
-            <div className="text-5xl z-10 max-lg:text-3xl max-md:text-base">
+        <div className="w-full h-screen max-lg:h-auto py-[10px] flex flex-col md:justify-between overflow-hidden">
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                viewport={{ once: true }}
+                className="text-5xl z-10 max-lg:text-3xl max-md:text-base max-md:hidden px-[30px] max-md:px-[10px]"
+            >
                 <p>“В России расстаются навсегда.</p>
                 <div className="w-full flex justify-center">
                     <p>В России друг от друга города</p>
@@ -20,8 +28,14 @@ export default function Home() {
                 <div className="w-full flex justify-end">
                     <p>БориС РЫЖИЙ</p>
                 </div>
-            </div>
-            <div className="relative bottom-20 z-0 left-0 right-0 m-auto my-0 max-md:top-[50px] h-[430px] w-[650px] max-md:h-[280px] max-md:w-[400px]">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                viewport={{ once: true }}
+                className="relative bottom-20 z-0 left-0 right-0 m-auto my-0 max-md:top-[50px] h-[430px] w-[650px] max-md:h-[400px] max-md:w-full"
+            >
                 <Image
                     src={"/images/main_image.png"}
                     alt="main_image"
@@ -29,8 +43,14 @@ export default function Home() {
                     fill={true}
                     sizes="auto"
                 />
-            </div>
-            <div className="absolute top-[50%] left-[10%] max-md:top-[28%] max-md:hidden">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                viewport={{ once: true }}
+                className="absolute top-[50%] left-[10%] max-md:top-[28%] max-md:hidden px-[30px] max-md:px-[10px]"
+            >
                 <p className="font-black text-accent max-md:text-sm">
                     [ русский поэт ]
                 </p>
@@ -38,10 +58,20 @@ export default function Home() {
                     <p className="text-gray text-sm">годы:</p>
                     <p>1974 — 2001</p>
                 </div>
-            </div>
-            <div className="w-full justify-between max-md:px-[5px] flex items-center max-lg:items-start md:px-[5%] py-[10px] max-lg:flex-col max-md:mt-[100px]">
-                <img className="md:hidden" src="images/main_text.svg" alt="" />
-                <div className="md:hidden w-full flex  flex-col mt-[20px]">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                viewport={{ once: true }}
+                className="w-full justify-between flex items-center max-lg:items-start md:px-[5%] py-[10px] max-lg:flex-col max-md:mt-[90px] max-md:px-[5px]"
+            >
+                <img
+                    className="md:hidden max-md:px-4"
+                    src="images/main_text.svg"
+                    alt=""
+                />
+                <div className="md:hidden w-full flex  flex-col mt-[20px] max-md:px-4">
                     <p className="font-black text-accent max-md:text-sm">
                         [ русский поэт ]
                     </p>
@@ -55,7 +85,7 @@ export default function Home() {
                         <Link key={id} href={item.link}>
                             <Button
                                 variant="ghost"
-                                className="text-xl max-md:text-base max-md:pl-0"
+                                className="text-xl max-md:text-xl"
                             >
                                 {item.title}
                             </Button>
@@ -65,14 +95,14 @@ export default function Home() {
                 <div className="flex items-center min-w-[100px] w-auto h-auto">
                     <Button
                         variant="ghost"
-                        className="text-xl hover:text-transparent max-md:pl-0"
+                        className="text-xl hover:text-transparent"
                     >
                         <p className="text-accent text-sm font-black">
                             о сайте
                         </p>
                     </Button>
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 }
