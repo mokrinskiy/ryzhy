09b96642970dcb7cdf29e050162cf813aca795b3
mokrinskiy <artyommokrinskiy@gmail.com>
2025-02-06 20:30:40 +0700

    fix v7

--- app/poems/[slug]/page.tsx
+++ app/poems/[slug]/page.tsx
@@ -1,32 +1,29 @@
 import { poems } from "@/content/poems";
 import React from "react";
 
-export default function Page({ params }: { params: { slug: string } }) {
-    const index = Number(params.slug) - 1;
-    const item = poems[index];
-
-    if (!item) {
-        return (
-            <div className="text-center text-red-500">
-                Стихотворение не найдено
-            </div>
-        );
-    }
-
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ slug: string }>;
+}) {
+    const slug = (await params).slug;
     return (
         <div className="w-full h-auto py-[50px] flex justify-center items-center max-md:px-[10px] overflow-hidden">
             <div className="flex flex-col items-center px-[10px]">
-                <h1 className="text-xl">{item.title}</h1>
+                <h1 className="text-xl">{poems[parseInt(slug) - 1].title}</h1>
                 <div>
                     <p
                         className="mt-[20px] text-base whitespace-pre-line"
                         dangerouslySetInnerHTML={{
-                            __html: item.text.replace(/\n/g, "<br />"),
+                            __html: poems[parseInt(slug) - 1].text.replace(
+                                /\n/g,
+                                "<br />"
+                            ),
                         }}
                     />
-                    {item.year && (
+                    {poems[parseInt(slug) - 1].year && (
                         <p className="mt-[10px] text-sm text-accent w-full">
-                            {item.year} год
+                            {poems[parseInt(slug) - 1].year} год
                         </p>
                     )}
                 </div>
